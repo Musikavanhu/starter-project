b12f29962f138418f37e77bdb738ac9c432fc650
Tinotenda Musikavanhu <thesafehavenhero@gmail.com>
2021-06-27 11:09:07 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,30 +1,17 @@
-import './App.css';
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
+import Main from "./components/Main";
+import Wrapper from "./components/Wrapper";
+import Header from "./components/Header";
+import "./App.css";
 
-class Home extends React.Component {
-    render() {
-        return (
-            <div className="App">
-                <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-                </header>
-            </div>
-            
-        );
-    }
-
+function App(){
+return (
+<div className="App">
+  <Wrapper>
+    <Header />
+    <Main />
+  </Wrapper>
+</div>
+);
 }
-
-export default Home;
+export default App;
